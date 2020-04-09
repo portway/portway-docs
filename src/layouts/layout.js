bdac5c93798409cbb5bbba18c941d1dd9b810583
Jay Contonio <jay@conton.io>
2020-04-09 10:24:22 -0700

    Removing Gatsby's meta tags; Updating SEO

--- src/layouts/layout.js
+++ src/layouts/layout.js
@@ -6,14 +6,14 @@ import SEO from '../components/seo'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
 
-const Layout = ({ children, className, title }) => {
+const Layout = ({ children, className, description, title }) => {
   const layoutClasses = cx({
     'container': true,
     [className]: className
   })
   return (
     <div className="document">
-      <SEO title={title} />
+      <SEO title={title} description={description} />
       <Header />
       <main className={layoutClasses}>
         {children}
@@ -26,6 +26,7 @@ const Layout = ({ children, className, title }) => {
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
+  description: PropTypes.string,
   title: PropTypes.string,
 }
 
