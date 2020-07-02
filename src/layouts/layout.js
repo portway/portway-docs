fb2c925e1d98599ac0ef0171ae249054531e2627
Jay Contonio <jay@conton.io>
2020-07-02 10:35:54 -0700

    Adding social image and better url tags

--- src/layouts/layout.js
+++ src/layouts/layout.js
@@ -6,14 +6,18 @@ import SEO from '../components/seo'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
 
-const Layout = ({ children, className, description, title }) => {
+const Layout = ({ children, className, description, location, title }) => {
   const layoutClasses = cx({
     'container': true,
     [className]: className
   })
   return (
     <div className="document">
-      <SEO title={title} description={description} />
+      <SEO
+        title={title}
+        description={description}
+        location={location}
+      />
       <Header />
       <main className={layoutClasses}>
         {children}
@@ -27,6 +31,7 @@ Layout.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
   description: PropTypes.string,
+  location: PropTypes.object.isRequired,
   title: PropTypes.string,
 }
 
